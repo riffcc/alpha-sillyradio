defe2917ea73ed9d1230c09b0ab036e548f35250
Benjamin Arntzen <zorlin@gmail.com>
2021-04-04 10:55:02 +0200

    Fix paths

--- create-qbt.py
+++ create-qbt.py
@@ -31,8 +31,8 @@ for i in range(1,howManyRadios+1):
     volumes:
       - /mfsbrick.3/final{radioNum}:/downloads
       - /mnt/radio/config{radioNum}:/config
-      - /mnt/radio/watch{radioNum}:/mnt/watch
-      - /mnt/radio/incomplete{radioNum}:/mnt/incomplete
+      - /mnt/radio/watch{radioNum}:/watch
+      - /mnt/radio/incomplete{radioNum}:/incomplete
     ports:
       - "{portANum}:8080/tcp"
       - "{portBNum}:6881"
